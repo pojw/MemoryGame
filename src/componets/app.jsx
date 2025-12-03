768eaa62ff42b53e1e1dcf0d65ed831787a25200
jaylin <jaylinhernandez123@icloud.com>
2025-12-03 14:22:56 -0500

    set up the correct structure of the patge and implented the first row of infomration

--- src/componets/app.jsx
+++ src/componets/app.jsx
@@ -1,11 +1,15 @@
 //Main Page
 import {  useEffect , useState } from "react"
 import Card from "./card"
-
+import Details from "./details"
 export default function CharacterList(){
     const [people, setPeople]=useState([])
     const [type,setType]=useState('Paramecia')
     let[ click,setClick] =useState([])
+    let [mode, setMode] = useState(8)
+    let [fruits,setFruits]= useState([])
+    let [currentScore, setCurrentScore]=useState(0)
+    let [highScore,setHighScore]=useState(0)
 
 
 
@@ -15,9 +19,25 @@ useEffect(()=>{
         try{
             let response= await fetch("https://api.api-onepiece.com/v2/fruits/en")
             let data=await response.json()
+            let index=0
+            let newFruits=[]
+            while(newFruits.length<mode){
+                if(data[index].type==type){
+                    let card={
+                        name:data[index].name,
+                        id:data[index].id,
+                        filename:data[index].filename,
+                        type:data[index].type
+                    }
+                                        newFruits.push(card)
+
+                }
+                              index+=1
+                }
             
             console.log("Api results", data)
-            setPeople(data)
+            console.log(",", newFruits)
+            setFruits(newFruits)
         }
         catch(error){
             console.log("Error Fetching " ,error)
@@ -25,31 +45,15 @@ useEffect(()=>{
 
     }
     fetchPeople();
-},[type])
+},[type,mode])
 
-function handleClick(index){
-    if(!click.includes(index)){
-        alert("good")
-        let clone=[...click,index]
-        
-        setClick(clone)
-    }
-}
 
 return (
-   <div>
-    <div>{type}</div>
-    {people.filter((value)=>value.type===type).slice(4,7).map((filtred)=>
-  
-    (
-     <Card name={filtred.name} url={filtred.filename} onclick={()=>{handleClick(filtred.id)}}></Card>
-    ))
+<div className="container">
+    <Details currentScore={currentScore} highScore={highScore}></Details>
+    <div  className="cardSection"></div>
+    <div className="options"></div>
 
-    }
-
-
-   </div>
-
-
+</div>
 )
 }
---
+++ src/componets/details.jsx
@@ -0,0 +1,19 @@
+
+
+
+export default function Details({currentScore,highScore}){
+
+
+    return(
+        <div className="details"> 
+
+        <div>
+            <div>Score {currentScore}</div>
+            <div>Highest Score {highScore}</div>
+        </div>
+
+        </div>
+    )
+
+
+}
